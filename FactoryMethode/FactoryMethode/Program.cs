019cf835776f267df672acc20f9514fe1e166492
Mykola <m.drobush@istu.edu.ua>
2023-11-29 22:45:57 +0200

    Implementation factory method, added gitignore

---
+++ FactoryMethode/FactoryMethode/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+// Інтерфейс для створення об'єктів книг
+interface IBook
+{
+    void Display();
+}
+
+// Конкретний клас книги
+class MysteryBook : IBook
+{
+    public void Display()
+    {
+        Console.WriteLine("Це книга жанру Містика.");
+    }
+}
+
+// Конкретний клас книги
+class RomanceBook : IBook
+{
+    public void Display()
+    {
+        Console.WriteLine("Це книга жанру Романтика.");
+    }
+}
+
+// Фабричний метод визначений у віддільному інтерфейсі
+interface IBookFactory
+{
+    IBook CreateBook();
+}
+
+// Конкретна фабрика для створення книги жанру Містика
+class MysteryBookFactory : IBookFactory
+{
+    public IBook CreateBook()
+    {
+        return new MysteryBook();
+    }
+}
+
+// Конкретна фабрика для створення книги жанру Романтика
+class RomanceBookFactory : IBookFactory
+{
+    public IBook CreateBook()
+    {
+        return new RomanceBook();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Використання фабричних методів для створення об'єктів книг
+        IBookFactory mysteryBookFactory = new MysteryBookFactory();
+        IBook mysteryBook = mysteryBookFactory.CreateBook();
+        mysteryBook.Display();
+
+        IBookFactory romanceBookFactory = new RomanceBookFactory();
+        IBook romanceBook = romanceBookFactory.CreateBook();
+        romanceBook.Display();
+    }
+}
